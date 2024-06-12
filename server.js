ee115a9e12322acbcd313d01f3de7e6ae95efd3d
Ayush12521198 <86072815+Ayush12521198@users.noreply.github.com>
2024-06-12 19:33:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
-const path = require("path");
+// const path = require("path");
 const connectDb = require("./config/connectDb");
-
 // config dot env file
 dotenv.config();
 
@@ -22,28 +20,17 @@ app.use(express.json());
 app.use(cors());
 
 //routes
+
+// app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/users", require("./routes/userRoute"));
+
+// //transections routes
+// app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 app.use("/transections", require("./routes/transectionRoutes"));
 
-// Define root route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Home Page!');
-});
-
-// Define /login route
-app.get('/login', (req, res) => {
-  res.send('Login Page');
-});
-
-// Static files
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
 
 //port
-const PORT = process.env.PORT || 8080;
+const PORT = 8080 || process.env.PORT;
 
 //listen server
 app.listen(PORT, () => {
